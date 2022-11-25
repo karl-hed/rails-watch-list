8a5356417513aa632fe1ad4d3b4b1265b22f021a
Karl Hedayati <k.hedayati.10@gmail.com>
2022-11-25 14:33:11 -0500

    uploading from user to cloudinary ok with local

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,7 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   validates :name, uniqueness: true
   validates :name, presence: true
+  has_one_attached :photo
 end
 # List is a category
 # Bookmark is a movie
